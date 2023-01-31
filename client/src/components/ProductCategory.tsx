150e29a0994a9f8176d609973c28a59601f59e2d
phannsothyro <phannsothyro@gmail.com>
2023-01-31 17:10:01 +0700

    product category card

---
+++ client/src/components/ProductCategory.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Flex,
+  Avatar,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+import { BiChat, BiLike, BiShare } from "react-icons/bi";
+
+const ProductCategory = () => {
+  return (
+    <Card maxW="md">
+      <Image
+        objectFit="cover"
+        src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        alt="Chakra UI"
+      />
+      <Text className="text">Hello</Text>
+    </Card>
+  );
+};
+
+export default ProductCategory;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Layout from "../components/Layout";
+import ProductCategory from "../components/ProductCategory";
 import { UserContext } from "../contexts/UserContext";
+import { Container, Grid, GridItem, Text } from "@chakra-ui/react";
 
 const Home = () => {
-  return <Layout>Hello World</Layout>;
+  return (
+    <Layout>
+      <Container maxW={"8xl"} py={2}>
+        <h1>Product Categories</h1>
+        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+          <ProductCategory />
+          <ProductCategory />
+          <ProductCategory />
+          <ProductCategory />
+          <ProductCategory />
+          <ProductCategory />
+        </Grid>
+      </Container>
+    </Layout>
+  );
 };
 
 export default Home;
