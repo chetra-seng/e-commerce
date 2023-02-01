8f64ca480491ca4218239cfb75eee5d59110b168
Lyheng <senglyheng2001@gmail.com>
2023-02-01 16:06:06 +0700

    create Category

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -19,6 +19,7 @@ const Home = () => {
           <ProductCategory />
           <ProductCategory />
         </Grid>
+        <h1>dfsgaiufgsdiufgsiadfiusdafiasdfggif</h1>
       </Container>
     </Layout>
   );
