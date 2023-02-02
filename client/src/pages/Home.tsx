779b6b385173ad4768b67e90ce01244bbb04784c
Lyheng <senglyheng2001@gmail.com>
2023-02-02 13:21:27 +0700

    update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,32 +9,45 @@ import TrendingCard from "../components/TrendingCard";
 
 const Home = () => {
   return (
-    <Layout>
-      <Container maxW={"8xl"} pt={2} pb={10} borderBottom={"1px"} borderColor={"gray.200"}>
-        <Text className="mt-10 mb-5" fontWeight={"bold"}>Product Categories</Text>
-        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-          <ProductCategory />
-          <ProductCategory />
-          <ProductCategory />
-          <ProductCategory />
-          <ProductCategory />
-          <ProductCategory />
-        </Grid>
-      </Container>
-      <Container maxW={"8xl"} mb={"20"}>
-        <Text className="mt-10 mb-5" fontWeight={"bold"}>In Trending</Text>
-        <Grid templateColumns="repeat(4, 1fr)" gap={8}>
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-          <TrendingCard />
-        </Grid>
-      </Container>
-    </Layout>
+    <>
+      <Head title={"Home Page"} />
+      <Layout>
+        <Container
+          maxW={"8xl"}
+          pt={2}
+          pb={10}
+          borderBottom={"1px"}
+          borderColor={"gray.200"}
+        >
+          <Text className="mt-10 mb-5" fontWeight={"bold"}>
+            Product Categories
+          </Text>
+          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <ProductCategory />
+            <ProductCategory />
+            <ProductCategory />
+            <ProductCategory />
+            <ProductCategory />
+            <ProductCategory />
+          </Grid>
+        </Container>
+        <Container maxW={"8xl"} mb={"20"}>
+          <Text className="mt-10 mb-5" fontWeight={"bold"}>
+            In Trending
+          </Text>
+          <Grid templateColumns="repeat(4, 1fr)" gap={8}>
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+            <TrendingCard />
+          </Grid>
+        </Container>
+      </Layout>
+    </>
   );
 };
 
