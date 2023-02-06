30d9c927019003caf667a0d8f9641faf98c650cf
Lyheng <senglyheng2001@gmail.com>
2023-02-06 10:46:27 +0700

    Update Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,12 +4,39 @@ import Head from "../components/Head";
 import Layout from "../components/Layout";
 import ProductCategory from "../components/ProductCategory";
 import { UserContext } from "../contexts/UserContext";
-import { Container, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import TrendingCard from "../components/TrendingCard";
 
 const Home = () => {
   return (
     <Layout>
+      <Container maxW={"8xl"} h={"680"}>
+        <Flex
+          boxShadow='base' p='6' rounded='md'
+          alignItems={"center"}
+          justify={"space-between"}
+          padding={"50"}
+        >
+          <div style={{ width: "300px" }}>
+            <Text className="text-center" fontWeight={"bold"} fontSize="3xl">
+              Healine text
+            </Text>
+            <Text className="text-center">
+              Lorem ipsum dolor sit amet consectetur. Eleifend luctus placerat
+              fermentum bibendum egestas at mi. Lobortis suspendisse at posuere
+              nunc.
+            </Text>
+          </div>
+          <div className="max-w-3xl">
+            <img
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt=""
+              className="rounded-md"
+            />
+          </div>
+        </Flex>
+      </Container>
+
       <Container maxW={"8xl"} py={2}>
         <Text className="mt-10 mb-5" fontWeight={"bold"}>
           Product Categories
