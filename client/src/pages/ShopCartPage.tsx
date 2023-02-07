8bee899289c590c356ec91016cbbbcfb4a404dca
Lyheng <senglyheng2001@gmail.com>
2023-02-07 16:52:27 +0700

    Update cart

--- client/src/pages/ShopCartPage.tsx
+++ client/src/pages/ShopCartPage.tsx
@@ -1,4 +1,12 @@
-import { Box, Center, Container, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import Head from "../components/Head";
 import Layout from "../components/Layout";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
@@ -23,7 +31,7 @@ export default function ShopCartPage() {
         <Flex justify={"space-around"}>
           <Box w={"xl"}>
             <Box w={"2xl"} h="180 " marginBottom={"6"} boxShadow="base ">
-              <Flex >
+              <Flex>
                 <Image
                   src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -32,9 +40,7 @@ export default function ShopCartPage() {
                 />
                 <div className="p-5">
                   <h1 className="text-lg font-bold">XX.XX$</h1>
-                  <p className="mt-5">
-                    PREDATOR PRECISION.3 FIRM GROUND BOOTS
-                  </p>
+                  <p className="mt-5">PREDATOR PRECISION.3 FIRM GROUND BOOTS</p>
                   <Flex className="mt-5">
                     <div>
                       <AiOutlineMinus className="text-2xl border p-1 rounded-full " />
@@ -73,7 +79,29 @@ export default function ShopCartPage() {
               </Flex>
             </Box>
           </Box>
-          <Box w={"md"} h="10" bg="red.500"></Box>
+          <Box w={"md"}  boxShadow="base " h="300 ">
+            <div className="p-5">
+              <h1 className="text-2xl font-bold">Order Summary</h1>
+              <Flex justify={"space-between"} marginTop={'5'}  >
+                <h1>3 Item</h1>
+                <h1>XX.XX$</h1>
+              </Flex>
+              <Flex justify={"space-between"} marginTop={'5'}  >
+                <h1>Delivery</h1>
+                <h1>XX.XX$</h1>
+              </Flex>
+              <hr className="mt-5"/>
+              <Flex justify={"space-between"} marginTop={'5'} fontWeight={"bold"} fontSize="xl" >
+                <h1>Delivery</h1>
+                <h1>XX.XX$</h1>
+              </Flex>
+              <Flex justify={'center'} >
+                <Button colorScheme="gray" w={400} className="mt-5">
+                  Continue Checkout
+                </Button>
+              </Flex>
+            </div>
+          </Box>
         </Flex>
       </Layout>
     </>
