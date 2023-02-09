4b6b11a8324ec26ee0b725e2c928990e7f200187
Lyheng <senglyheng2001@gmail.com>
2023-02-09 16:17:21 +0700

    Update

--- client/src/pages/seller/Categories.tsx
+++ client/src/pages/seller/Categories.tsx
@@ -1,15 +1,62 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  Flex,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useRef } from "react";
 import Head from "../../components/Head";
 import DeshboardSidebar from "../../components/seller/DeshboardSidebar";
 
 export default function CategoriesPage() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef()
+
   return (
     <>
       <Head title={"Categories"} />
       <Flex>
         <DeshboardSidebar />
         <Box className="w-5/6">
-          
+          <Flex justify={"center"} alignItems={"center"} h="100vh ">
+            <Text fontWeight={"bold"} fontSize="6xl">
+              <Button onClick={onOpen}>Discard</Button>
+              <AlertDialog
+                motionPreset="slideInBottom"
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                isOpen={isOpen}
+                isCentered
+              >
+                <AlertDialogOverlay />
+
+                <AlertDialogContent>
+                  <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+                  <AlertDialogCloseButton />
+                  <AlertDialogBody>
+                    Are you sure you want to discard all of your notes? 44 words
+                    will be deleted.
+                  </AlertDialogBody>
+                  <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                      No
+                    </Button>
+                    <Button colorScheme="red" ml={3}>
+                      Yes
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </Text>
+          </Flex>
         </Box>
       </Flex>
     </>
